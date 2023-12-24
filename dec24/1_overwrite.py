35e1ab1537f6f792494de7097b007b936d82b7c4
Mohammed Saqlain <85921230+Mohammedsaklain@users.noreply.github.com>
2023-12-24 14:30:03 +0530

    Update 1_overwrite.py

--- dec24/1_overwrite.py
+++ dec24/1_overwrite.py
@@ -1,6 +1,7 @@
-# in this code , every time a new code is overwritten rather than printing in same file
+# save inside a result folder
 
 import openai
+import os
 
 model_engine = "text-davinci-003"
 openai.api_key = "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"
@@ -15,18 +16,27 @@ def GPT(query):
     
     return response['choices'][0]['text'].strip()
 
+result_folder = 'result'
+if not os.path.exists(result_folder):
+    os.makedirs(result_folder)  # Create the result directory if it doesn't exist
+
 exit_words = ("q", "Q", "quit", "QUIT", "EXIT")
 
 try:
     while True:
-        query = input()  # Removed the prompt to make it cleaner
+        query = input()
         if query in exit_words:
             break  # Ends the chat without printing "ENDING CHAT"
         else:
             answer = GPT(query)
             print(answer)  # Only prints the answer
             
-            with open('result.io', 'w') as file:  # Use 'w' mode to overwrite the existing content
+            # Define the path to the .ino file within the result folder
+            file_path = os.path.join(result_folder, 'result.ino')
+            
+            # Write the answer to the result.ino file in the result folder
+            with open(file_path, 'w') as file:  # Use 'w' mode to overwrite the existing content
                 file.write(answer + "\n")  # File only logs the answer.
+
 except KeyboardInterrupt:
     print()  # Prints a new line for clean exit
